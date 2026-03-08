3cef3684f1aeece7e4db675104e27ef6d3d53d87
dc6r52r5c6-ctrl <dc6r52r5c6@privaterelay.appleid.com>
2026-03-08 19:19:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
 8663322759:AAHbU0hf9fCPxP65oVAqpkXEoqJBO10WYfQ
+import telebot
+
+TOKEN = "ТУТ_НОВЫЙ_ТОКЕН"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Бот работает 🚀")
+
+bot.polling()
